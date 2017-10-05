d181dcd758caed4bc07b366c1ee4b6b0680216ef
Ivan <doktor32.dok@mail.ru>
2017-10-05 14:45:29 +0300

    Add files via upload

---
+++ Scripts/Gameplay/Utilities/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utilities
+{
+    #region TAGS
+    public static string PLAYER_BUILDING_TAG = "PlayerBuilding";
+    public static string BOOST_TAG = "Boost";
+    #endregion
+
+    #region PLAYER_PREFS
+    public static string PP_SHOP_DATA = "PP_SHOP_DATA";
+    public static string PP_SOUND_SETTINGS = "PP_SOUND_SETTINGS";
+    public static string PP_PLAYER_DATA = "PP_PLAYER_DATA";
+    #endregion
+
+    #region SCENES
+    public static string SCENE_MENUSCENE = "MenuScene";
+    #endregion
+
+    #region URL
+    public static string COMPANY_URL = "www.aporteinteractive.com";
+    public static string STORE_GAME_URL_ANDROID = "https://play.google.com/store/apps/details?id=com.aporte.kindwar"; //somethign like this https://play.google.com/store/apps/details?id=com.yourcompanyname.gamename
+    public static string STORE_GAME_URL_IOS = "http://itunes.apple.com/app/kind-war/id1175854473";//"www.google.com"; //something like: "http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=<your app number here>&mt=8
+    #endregion
+
+    #region LEADERBOARD_LINKS
+    public static string iOSLeaderabordString = "_116619"; //here you will ahve to insert ios leaderboard string.
+    #endregion
+}
